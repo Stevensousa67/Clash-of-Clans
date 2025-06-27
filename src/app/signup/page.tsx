25405827effdafab770aae976841844ef6a93cce
Steven Sousa <silver.steven@hotmail.com>
2025-06-27 10:25:58 -0400

    created login and signup pages

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -4,7 +4,7 @@ export default function LoginPage() {
   return (
     <div className="flex flex-col items-center justify-center p-6 md:p-10 mt-20">
       <div className="w-full max-w-sm md:max-w-3xl">
-        <AuthForm formType="login" title="Welcome back!" subtitle="Login to your account" submitButtonText="Login" linkText="Don't have an account?" linkUrl="/signup" showForgotPassword={true} />
+        <AuthForm formType="signup" title="Create an account" subtitle="Signup to get started" submitButtonText="Sign Up" linkText="Already have an account?" linkUrl="/login" />
       </div>
     </div>
   )
