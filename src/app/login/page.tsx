d2d72c06e87911c86662cce720d785ea1ab39e19
Steven Sousa <silver.steven@hotmail.com>
2025-06-27 10:29:50 -0400

    adjusted mt

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@ import { AuthForm } from "@/components/AuthForm"
 
 export default function LoginPage() {
   return (
-    <div className="flex flex-col items-center justify-center p-6 md:p-10 mt-20">
+    <div className="flex flex-col items-center justify-center p-6 md:p-10 mt-45">
       <div className="w-full max-w-sm md:max-w-3xl">
         <AuthForm formType="login" title="Welcome back!" subtitle="Login to your account" submitButtonText="Login" linkText="Don't have an account?" linkUrl="/signup" showForgotPassword={true} />
       </div>
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,7 +2,7 @@ import { AuthForm } from "@/components/AuthForm"
 
 export default function LoginPage() {
   return (
-    <div className="flex flex-col items-center justify-center p-6 md:p-10 mt-20">
+    <div className="flex flex-col items-center justify-center p-6 md:p-10 mt-35">
       <div className="w-full max-w-sm md:max-w-3xl">
         <AuthForm formType="signup" title="Create an account" subtitle="Signup to get started" submitButtonText="Sign Up" linkText="Already have an account?" linkUrl="/login" />
       </div>
