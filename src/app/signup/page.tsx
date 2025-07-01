8af07122ff2b9948d5593ed30ae3667bbabb6b34
Steven Sousa <silver.steven@hotmail.com>
2025-07-01 07:44:54 -0400

    centered AuthForms to viewport

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,7 +2,7 @@ import { AuthForm } from "@/components/auth/components/AuthForm"
 
 export default function LoginPage() {
   return (
-    <div className="flex flex-col items-center justify-center p-6 md:p-10 mt-35">
+    <div className="flex flex-col items-center justify-center my-auto p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
         <AuthForm formType="signup" title="Create an account" subtitle="Signup to get started" submitButtonText="Sign Up" linkText="Already have an account?" linkUrl="/login" />
       </div>
