ac0fc4d6d3f75ee22af56c625a5959982596ac7d
Steven Sousa <silver.steven@hotmail.com>
2025-07-02 13:04:33 -0400

    moved auth related pages into (auth)

--- src/app/(auth)/forgotPassword/page.tsx
+++ src/app/(auth)/forgotPassword/page.tsx
@@ -4,7 +4,7 @@ export default function forgotPasswordPage() {
   return (
     <div className="flex flex-col items-center justify-center my-auto p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
-        <AuthForm formType="forgotPassword" title="Forgot Password" subtitle="Reset your password" submitButtonText="Reset Password" linkText="Remembered your password?" linkUrl="/login" />
+        <AuthForm formType="forgotPassword" title="Forgot Password" subtitle="Reset your password" submitButtonText="Reset" linkText="Remembered your password?" linkUrl="/login" />
       </div>
     </div>
   )
