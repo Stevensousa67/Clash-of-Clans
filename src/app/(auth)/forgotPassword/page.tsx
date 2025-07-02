636bb497b8bc6976bb3267b742486e26b17258c8
Steven Sousa <silver.steven@hotmail.com>
2025-07-02 15:43:03 -0400

    used min-h-screen for auth pages

--- src/app/(auth)/forgotPassword/page.tsx
+++ src/app/(auth)/forgotPassword/page.tsx
@@ -2,7 +2,7 @@ import { AuthForm } from "@/components/auth/components/AuthForm"
 
 export default function forgotPasswordPage() {
   return (
-    <div className="flex flex-col items-center justify-center my-auto p-6 md:p-10">
+    <div className="flex flex-col items-center justify-center min-h-screen p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
         <AuthForm formType="forgotPassword" title="Forgot Password" subtitle="Reset your password" submitButtonText="Reset" linkText="Remembered your password?" linkUrl="/login" />
       </div>
